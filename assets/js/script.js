31420e7c46eb977779e5125311885b4f47741fec
Ianka Lopes <iankalp50@gmail.com>
2025-02-12 14:01:05 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,44 +3,54 @@ const formulario = document.querySelector("#formulario");
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 async function getApiGithub() {
-    try {
+    try{
+        
         const dadosPerfil = await fetch(`https://api.github.com/users/iankalps`);
         const perfil = await dadosPerfil.json();
 
-        sobre.innerHTML = `
+        let conteudo = `
             <img
-                src="${perfil.avatar_url}"
-                alt="Foto do Perfil do GitHub - ${perfil.name}"
+            src="${perfil.avatar_url}"
+            alt="Foto do Perfil do GitHub - ${perfil.name}"
             />
 
             <article id="about_texto">
-                <h2>Sobre mim</h2>
+            <h2>Sobre mim</h2>
                 <p>Olá! Meu nome é Ianka Lopes e sou uma desenvolvedora fullstack focada em tecnologia, design e soluções criativas. 
                     Com formação técnica em TI, venho aprimorando minhas habilidades em desenvolvimento web, especialmente com JavaScript, 
                     TypeScript, Node.js, Nest.js e React.js. Atualmente, estou me dedicando ao bootcamp da Generation Brasil para evoluir 
                     na carreira. Sou organizada, proativa e valorizo o trabalho em equipe. Meu objetivo é criar soluções digitais eficientes 
                     e impactantes, sempre buscando aprendizado e crescimento.</p>
-                
-                <div class="flex about_github">
-                    <a class="botao" href="${perfil.html_url}" target="_blank">GitHub</a>
+            
+            <div class="flex about_github">
+
+                <a 
+                    class="botao" 
+                    href="${perfil.html_url}" 
+                    target="_blank"
+                >
+                    GitHub
+                </a>
                     <p>${perfil.followers} Seguidores</p>
                     <p>${perfil.public_repos} Repositórios</p>
-                </div>
+            </div>
+
             </article>
         `;
-    } catch (error) {
-        console.error("Erro ao carregar os dados do GitHub:", error);
-        sobre.innerHTML = "<p>Não foi possível carregar os dados do GitHub.</p>";
+
+        sobre.innerHTML += conteudo;
+    } catch(error) {
+        console.error("Error:", error);
     }
 }
 
-formulario.addEventListener("submit", function (event) {
+formulario.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const campoNome = document.querySelector("#nome");
-    const txtNome = document.querySelector("#txtNome");
+    const txtNome = document.querySelector("#txtNome")
 
-    if (campoNome.value.trim().length < 3) {
+    if(campoNome.value.length < 3){
         txtNome.innerHTML = "O nome deve ter no mínimo 3 caracteres.";
         campoNome.focus();
         return;
@@ -49,9 +59,9 @@ formulario.addEventListener("submit", function (event) {
     }
 
     const campoEmail = document.querySelector("#email");
-    const txtEmail = document.querySelector("#txtEmail");
+    const txtEmail = document.querySelector("#txtEmail")
 
-    if (!campoEmail.value.trim().match(emailRegex)) {
+    if(!campoEmail.value.match(emailRegex)){
         txtEmail.innerHTML = "Digite um e-mail válido.";
         campoEmail.focus();
         return;
@@ -60,9 +70,9 @@ formulario.addEventListener("submit", function (event) {
     }
 
     const campoAssunto = document.querySelector("#assunto");
-    const txtAssunto = document.querySelector("#txtAssunto");
+    const txtAssunto = document.querySelector("#txtAssunto")
 
-    if (campoAssunto.value.trim().length < 5) {
+    if(campoAssunto.value.length < 5){
         txtAssunto.innerHTML = "O assunto deve ter no mínimo 5 caracteres.";
         campoAssunto.focus();
         return;
